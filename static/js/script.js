1b0da5347f0e644d90c34e2ec809ebd492bad86e
gitzart <windowxpxp@gmail.com>
2017-02-24 16:38:17 +0630

    Add initial webpage rendering test

---
+++ static/js/script.js
@@ -0,0 +1,36 @@
+(function() {
+  'use strict';
+
+  var containerFluid = $('.container-fluid'),
+      logoutButton = $('.logout'),
+      navDrawer = $('.nav__drawer'),
+      drawerButton = $('.drawer-button'),
+      obfuscator = $('.obfuscator');
+
+  logoutButton.click(logout);
+  drawerButton.click(triggerNavDrawer);
+  obfuscator.click(triggerNavDrawer);
+
+  // Log the user out
+  function logout(e) {
+    e.preventDefault();
+    $.ajax({
+      method: 'POST',
+      url: $(e.target).attr('href')
+    })
+      .done(function(data) {
+        window.location = '/';
+      });
+  }
+
+  // Slide out the navigation drawer
+  function triggerNavDrawer(e) {
+    var expandedAria = true ? drawerButton.attr('aria-expanded') == "false" : false;
+    var hiddenAria = true ? navDrawer.attr('aria-hidden') == "false" : false;
+    drawerButton.attr('aria-expanded', expandedAria);
+    navDrawer.attr('aria-hidden', hiddenAria);
+    obfuscator.toggleClass('is-visible');
+    navDrawer.toggleClass('is-visible');
+    containerFluid.toggleClass('has-nav-drawer');
+  }
+})();
