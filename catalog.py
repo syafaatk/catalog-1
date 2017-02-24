abb77c4ad00de70e68d70bed83bfc21045a6fab9
gitzart <windowxpxp@gmail.com>
2017-02-24 16:31:17 +0630

    Add initial log In/Out system

--- catalog.py
+++ catalog.py
@@ -1,14 +1,44 @@
-from flask import Flask, render_template
+import os
+
+from flask import Flask, render_template, session, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
-app.config.from_object('config.DevConfig')
+app.config.from_object(os.environ.get('CONFIG', 'config.DevConfig'))
+
+db = SQLAlchemy(app)
+from models import Category, Item, User
+db.create_all()
+import load_db
 
 print(app.debug)
 print(app.secret_key)
 print(app.config['SQLALCHEMY_DATABASE_URI'])
 
 
-@app.route('/')
 def index():
-    return render_template('index.html')
+    u = User.query.get(session.get('uid', 0))
+    cats = Category.query.order_by(Category.name)
+    items = Item.query.order_by(Item.name)
+    return render_template('index.html', user=u, categories=cats, items=items)
+
+
+def login():
+    # Fake user sign in
+    if 'uid' not in session:
+        from random import randint
+        session['uid'] = randint(1, 8)
+    return redirect(url_for('index'))
+
+
+def logout():
+    session.pop('uid')
+    return redirect(url_for('index'))
+
+
+# URL rules
+app.add_url_rule('/', 'index', index)
+app.add_url_rule('/catalog/', 'index', index)
+app.add_url_rule('/login/', 'login', login)
+app.add_url_rule('/logout/', 'logout', logout, methods=['POST'])
