dee8ea2b96a2922bf0d3388e24b9f60a8daac3eb
gitzart <windowxpxp@gmail.com>
2017-02-27 01:37:41 +0630

    Polish index.html page

--- static/js/script.js
+++ static/js/script.js
@@ -3,13 +3,22 @@
 
   var body = $('body'),
       logoutButton = $('.logout'),
-      navDrawer = $('.nav__drawer'),
+      navDrawer = $('.nav-drawer'),
       drawerButton = $('.drawer-button'),
       obfuscator = $('.obfuscator');
 
   logoutButton.click(logout);
   drawerButton.click(triggerNavDrawer);
   obfuscator.click(triggerNavDrawer);
+
+  // Check screen resolution changes to toggle nav drawer
+  $(window).resize(function() {
+    var width = $(window).width(),
+        smScreen = 576;
+    if (width > smScreen && body.hasClass('has-nav-drawer')) {
+      triggerNavDrawer();
+    }
+  });
 
   // Log the user out
   function logout(e) {
@@ -26,9 +35,9 @@
   // Slide out the navigation drawer
   function triggerNavDrawer(e) {
     var expandedAria = true ? drawerButton.attr('aria-expanded') == "false" : false;
-    var hiddenAria = true ? navDrawer.attr('aria-hidden') == "false" : false;
+    var ndHiddenAria = true ? navDrawer.attr('aria-hidden') == "false" : false;
     drawerButton.attr('aria-expanded', expandedAria);
-    navDrawer.attr('aria-hidden', hiddenAria);
+    navDrawer.attr('aria-hidden', ndHiddenAria);
     obfuscator.toggleClass('is-visible');
     navDrawer.toggleClass('is-visible');
     body.toggleClass('has-nav-drawer');
