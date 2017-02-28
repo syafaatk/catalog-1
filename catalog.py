061f4236624a71570cac0cb6899c6ddfaaa7af57
gitzart <windowxpxp@gmail.com>
2017-02-28 14:52:20 +0630

    Add new HTML pages and items

--- catalog.py
+++ catalog.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask, render_template, session, redirect, url_for
+from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
 
 
@@ -19,8 +19,8 @@ print(app.config['SQLALCHEMY_DATABASE_URI'])
 
 def index():
     u = User.query.get(session.get('uid', 0))
-    cats = Category.query.order_by(Category.name)
-    items = Item.query.order_by(Item.name)
+    cats = Category.query.order_by(Category.name).all()
+    items = Item.query.order_by(Item.date_created.desc()).all()
     return render_template('index.html', user=u, categories=cats, items=items)
 
 
@@ -29,12 +29,33 @@ def login():
     if 'uid' not in session:
         from random import randint
         session['uid'] = randint(1, 8)
+    flash('You have been logged in.', 'info')
     return redirect(url_for('index'))
 
 
 def logout():
     session.pop('uid')
+    flash('You have been logged out.', 'info')
     return redirect(url_for('index'))
+
+
+def single_category(category):
+    u = User.query.get(session.get('uid', 0))
+    c = Category.query.filter_by(slug=category).first_or_404()
+    cats = Category.query.order_by(Category.name).all()
+    items = Item.query.filter_by(
+        category_id=c.id).order_by(Item.date_created.desc()).all()
+    return render_template('category.html', user=u, categories=cats,
+                           items=items, current_category=c)
+
+
+def single_item(category, item):
+    u = User.query.get(session.get('uid', 0))
+    c = Category.query.filter_by(slug=category).first_or_404()
+    i = Item.query.filter_by(slug=item, category_id=c.id).first_or_404()
+    cats = Category.query.order_by(Category.name).all()
+    return render_template('item.html', user=u, categories=cats,
+                           current_category=c, item=i)
 
 
 # URL rules
@@ -42,3 +63,5 @@ app.add_url_rule('/', 'index', index)
 app.add_url_rule('/catalog/', 'index', index)
 app.add_url_rule('/login/', 'login', login)
 app.add_url_rule('/logout/', 'logout', logout, methods=['POST'])
+app.add_url_rule('/catalog/<category>/', 'category', single_category)
+app.add_url_rule('/catalog/<category>/<item>/', 'item', single_item)
