792bd0fe4619db03d1b310db0c3abd3f3c399dd7
gitzart <windowxpxp@gmail.com>
2017-03-11 01:21:33 +0630

    Fix image path inconsistency

--- config.py
+++ config.py
@@ -5,16 +5,19 @@ Production and development mode configurations classes.
 
 import os
 
+from flask import request
+
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+BASE_URL = os.environ.get('BASE_URL', 'http://localhost:5000')
 DEV_DATABASE = 'sqlite:///{}'.format(os.path.join(BASE_DIR, 'catalog.db'))
 
 
 class Config(object):
     UPLOADS_DEFAULT_DEST = BASE_DIR + '/usercontent/img/'
-    UPLOADS_DEFAULT_URL = 'http://localhost:5000/usercontent/img/'
+    UPLOADS_DEFAULT_URL = BASE_URL + '/usercontent/img/'
     UPLOADED_IMAGES_DEST = BASE_DIR + '/usercontent/img/'
-    UPLOADED_IMAGES_URL = 'http://localhost:5000/usercontent/img/'
+    UPLOADED_IMAGES_URL = BASE_URL + '/usercontent/img/'
     WTF_CSRF_TIME_LIMIT = None
 
 
